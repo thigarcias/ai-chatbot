9e92312bb129294f639fc35c8da135bd7c498c11
ribgu <gustavo.ribeiro@sptech.school>
2025-02-10 23:10:33 -0300

    tira url mock

--- lib/ai/providers/copilot.ts
+++ lib/ai/providers/copilot.ts
@@ -2,5 +2,5 @@ import { createOpenAI } from '@ai-sdk/openai'
 
 export const copilot = createOpenAI({
     apiKey: 'a',
-    baseURL: 'http://localhost:3000/api/copilot/chat/completions',
+    baseURL: '/api/copilot/chat/completions',
 })
