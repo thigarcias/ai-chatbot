120163f7b45aa76c3b2281dd01195931f32f776d
ribgu <gustavo.ribeiro@sptech.school>
2025-03-02 22:49:00 -0300

    tira alguns tolltips

--- components/sidebar-toggle.tsx
+++ components/sidebar-toggle.tsx
@@ -1,10 +1,4 @@
-
 import { useSidebar } from '@/components/ui/sidebar'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/components/ui/tooltip'
 
 import { SidebarLeftIcon } from './icons'
 import { Button } from './ui/button'
@@ -13,17 +7,12 @@ export function SidebarToggle() {
   const { toggleSidebar } = useSidebar()
 
   return (
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <Button
-          onClick={toggleSidebar}
-          variant="outline"
-          className="md:px-2 md:h-fit"
-        >
-          <SidebarLeftIcon size={16} />
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent align="start">Toggle Sidebar</TooltipContent>
-    </Tooltip>
+    <Button
+      onClick={toggleSidebar}
+      variant="outline"
+      className="md:px-2 md:h-fit"
+    >
+      <SidebarLeftIcon size={16} />
+    </Button>
   )
 }
